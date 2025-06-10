f9ff8f27dd2a9c28bf1e75053c16627014af8c24
robertoahc85 <robertoahc85@gmail.com>
2025-06-10 17:51:34 -0600

    ejemplo clase y metodos

---
+++ modulo2/clases_y_metodos/coche.py
@@ -0,0 +1,26 @@
+class Coche:
+    def __init__(self, marca, modelo, color,):
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+        
+    def arrancar(self):
+        print(f"El coche {self.marca} {self.modelo} ha arrancado.")
+
+    def detener(self):
+        print(f"El coche {self.marca} {self.modelo} se ha detenido.")
+
+    def mostrar_info(self):
+        print(f"Coche: {self.marca} {self.modelo}, Color: {self.color}") 
+  
+#Vamos haer un lote de autos 
+marca= input("Dame la marca del coche: ")
+modelo =input("Dame el modelo del coche: ")
+color =input("Dame el color del coche: ")       
+mi_coche = Coche(marca, modelo, color)  
+mi_coche.mostrar_info()   
+mi_coche.arrancar()
+mi_coche.detener()
+el_coche = Coche("VW", "Citroneta", "Rojo")
+el_coche.mostrar_info()  
+            
