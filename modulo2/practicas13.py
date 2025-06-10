8bf6b94651b80549ed3ba9ec9ff08644139671fa
robertoahc85 <robertoahc85@gmail.com>
2025-06-09 18:26:21 -0600

    agrego practica 13

---
+++ modulo2/practicas13.py
@@ -0,0 +1,6 @@
+def __init__(self
+             ):
+    self.__nombre = "Practica 13"
+    self.__descripcion = "Ejercicios de repaso de funciones y estructuras de control"
+    self.__autor = "Data Science 25"
+    self.__version = "1.0.0"
