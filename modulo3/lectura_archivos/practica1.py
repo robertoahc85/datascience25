e5cbf97cce9869f99fd04424c276a5e0ef2c4f74
robertoahc85 <robertoahc85@gmail.com>
2025-06-17 18:42:18 -0600

    Agregando escribir csv

--- modulo3/lectura_archivos/practica1.py
+++ modulo3/lectura_archivos/practica1.py
@@ -1,4 +1,12 @@
 import pandas as pd
 # Cargar el archivo CSV
-df = pd.read_csv('entradas/estudiantes.csv',sep=";")
-print(df)
+# df = pd.read_csv('entradas/estudiantes.csv',sep=";")
+# print(df)
+# Eliminar filas valores nulos
+# df =pd.read_csv('entradas/ventas.csv', na_values=["N/A"] )
+# df = df.dropna()  # Eliminar filas donde 'Precio' es NaN
+# print(df)
+#escribir un archivo CSV
+data= {"Producto": ["Peras", "Durazno"],"Precio": [0.5, 0.3]}
+df = pd.DataFrame(data)
+df.to_csv('entradas/productos.csv', index=False)
