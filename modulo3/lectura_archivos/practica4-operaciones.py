5f5301e071679a897a5927e1d361099e9c3b1bd2
robertoahc85 <robertoahc85@gmail.com>
2025-06-19 18:01:29 -0600

    Estudiantes usan operacion panda

---
+++ modulo3/lectura_archivos/practica4-operaciones.py
@@ -0,0 +1,24 @@
+import pandas as pd
+# data = {"Nombre": ["Juan", "Ana", "Pedro"],
+#         "Curso": ["Matematicas", "Historia", "Ciencias"],
+#         "Calificacion": [90, 70, 70]}
+df_entrada = pd.read_csv("entradas/estudiantes_100.csv")
+df = pd.DataFrame(df_entrada)
+# print(df)
+print(df.describe())
+df.to_csv("salidas/estudiantes.csv", index=False)
+df_filtrado = df[df["Calificacion"] > 80]
+cal_min=df_filtrado['Calificacion'].min() 
+cal_max =df_filtrado['Calificacion'].max() 
+nuevo_min = 0
+nuevo_max = 10
+# df['Calif_0_10'] = (df['Calificacion'] - cal_min) / (cal_max - cal_min) * (nuevo_max - nuevo_min) + nuevo_min
+
+df_ordenado = df_filtrado.sort_values(by="Calificacion", ascending=False)
+print(df_ordenado)
+df_ordenado.to_csv("salidas/estudiantes_filtrados.csv", index=False)
+promedio_cursos = df["Calificacion"].mean()
+print(f"El promedio de las calificaciones es: {promedio_cursos:.2f}")
+promedio_cursos_por_curso = df.groupby("Curso")["Calificacion"].mean()
+print("Promedio de calificaciones por curso:")
+print(promedio_cursos_por_curso)
