b5a08301e3f4c837b6e89bf473f24804212416fd
robertoahc85 <robertoahc85@gmail.com>
2025-06-24 18:58:53 -0600

    Agregar agrupamiento

--- modulo3/lectura_archivos/practica8.py
+++ modulo3/lectura_archivos/practica8.py
@@ -13,3 +13,36 @@ print(df_ventas.head(9))
 df_multi_index = df_ventas.set_index(['Sucursal','Fecha', 'Categoria'])
 print("\n ------------Paso1. Multidex Aplicado ----------")
 print(df_multi_index.head(10))
+
+print("\n Busqueda con multindex .......")
+#Toda las venta en sucurcal 'Sur'
+print(df_multi_index.loc['Sur'])
+print("#########################")
+# #ventas centro el '2024-06-01'
+# print(df_multi_index.loc[('Sur','2024-01-01')])
+# print("---------%")
+#ventas del categoria Electrónica en el Sur y '2024-06-01'
+print(df_multi_index.loc['Sur','2024-01-01','Electrónica'])
+# ventas_elec = df_multi_index.xs('Electronica',level="Categoria")
+# # Resumen diario de todas las sucursusal solo electronica
+# print("\n -------Resumen diario------")-----
+# resumen_elec = (
+#     ventas_elec.assign.lambda...
+# )
+
+
+
+
+
+
+#Paso2 : Agrupamiento y agregacion
+#Agrupar por sucursal y categoria y luego vamos hacer operaciones
+df_grouped= df_ventas.groupby(['Sucursal','Categoria'])['Ventas'].sum().reset_index()
+print("\n ######## Paso2. agrupacion y agregacion ########")
+print(df_grouped.head(10))
+print("\n ######## Paso2. Ejemplo2 agrupacion y agregacion ########")
+df_grouped= df_ventas.groupby(['Sucursal','Categoria'])[['Ventas','Unidades']].agg(['sum','mean',"max","min"]).reset_index()
+print(df_grouped.head(10))
+
+#Paso3 . Transformacion  de estructura con Pivot
+
