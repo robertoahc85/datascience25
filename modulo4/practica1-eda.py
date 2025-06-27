6bd340968ff2d7688fb5ac156b24b6d7d30ff5f2
robertoahc85 <robertoahc85@gmail.com>
2025-06-26 18:38:44 -0600

    graficas de eda

--- modulo4/practica1-eda.py
+++ modulo4/practica1-eda.py
@@ -19,4 +19,47 @@ print("Filas duplicadas",df.duplicated().sum())
 print("Tipo de datos por columna")
 print(df.dtypes)
 
-#Paso4  Limpieza de lsod
+#Paso4  Limpieza de los datos
+df = df.drop_duplicates() #eliminar duplicados
+df['Edad'] = pd.to_numeric(df['Edad'] , errors='coerce')
+df['Edad'].fillna(df['Edad'].median(), inplace=True)
+df['Salario'].fillna(df['Salario'].median(), inplace=True)
+
+#Paso5 Estadistica descriptiva
+print("\n Medidas de tendecia central (Edad):")
+print("Media", df["Edad"].mean())
+print("Mediana", df["Edad"].median())
+print("Moda", df["Edad"].mode())
+
+print("\n Medidas de dispersion (Salario)")
+print("Rango", df["Salario"].max)
+print("Media", df["Salario"].median())
+print("Desviacion estandard:", df["Salario"].std())
+
+#Paso6 Visualizacion
+#5.1 Histogramas de edades
+sns.histplot(df['Edad'],kde=True)
+plt.title("Distribuccion de edad")
+plt.xlabel("Edad")
+plt.ylabel("Frencuencia")
+plt.show()
+
+
+#5.2 Boxplot de Salario
+sns.boxplot(x=df['Salario'])
+plt.title("Boxplot de Salario")
+plt.xlabel("Salario")
+plt.show()
+
+#5.3 Corelacion entre variables
+#Scatterplot de edad vs Salario
+sns.scatterplot(x='Edad', y='Salario', data=df)
+plt.title("Relacion entre Edad y Salario")
+plt.xlabel("Edad")
+plt.ylabel("Salario")
+plt.show()
+
+#5.4 pairplot(Analis Multivariado visual)
+sns.pairplot(df[['Edad','Salario']])
+plt.suptitle("Distribucion y relacion entre Edad y Salario", y=1.02)
+plt.show()
